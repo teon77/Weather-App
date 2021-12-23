9ee081b259b8f9e423ad279d2353b6aee4bad016
Eithan Refaely <eithanrefaely@gmail.com>
2021-12-23 22:02:40 +0200

    technical commit

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchCities = () => async (dispatch) => {
   const response = await axios.get(
-    "http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,11&appid=05dac35278852e949e793eb0d1fe08ec"
+    "https://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,11&appid=05dac35278852e949e793eb0d1fe08ec"
   );
   dispatch({ type: "FETCH_CITIES", payload: response.data.list });
 };
